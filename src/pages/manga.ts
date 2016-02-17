1a6c93a0105822ad3c9d0193a2d2e34d854070bd
Paarth <paarthk@gmail.com>
2016-02-17 16:12:26 -0500

    Removed unnecessary actions on manga page now that auto image replacement exists

--- src/pages/manga.ts
+++ src/pages/manga.ts
@@ -42,26 +42,6 @@ export class MangaPage extends RootPage {
 		});
 	}
 
-	@RegisteredAction({ id: 'pa_embiggen_manga_images', label: 'Embiggen' })
-	public embiggenImages(): void {
-		this.jQuery('img.image').toArray().forEach(image => {
-			let jQImage = this.jQuery(image);
-			// pixiv lazy loads image data, the full url is stored in data-src
-			// and copied over to the source attribute once a user comes into view.
-			let src = jQImage.attr('data-src');
-			let newSrc = pathUtils.getMaxSizeImageUrl(src);
-
-			log(`rewriting image src from [${src}] to [${newSrc}]`);
-
-			// Have to alter the data-src as well because if we don't, pixiv will 
-			// automatically copy over data-src again
-			jQImage.attr('data-src-backup', src);
-			jQImage.attr('data-src', newSrc);
-			jQImage.attr('src', newSrc);
-			jQImage.removeAttr('style');
-		});
-	}
-
 	@RegisteredAction({ id: 'pa_reverse_embiggen_manga_images', label: 'Reverse Embiggen' })
 	public reverseEmbiggen(): void {
 		this.jQuery('img.image').toArray().forEach(image => {
@@ -69,30 +49,6 @@ export class MangaPage extends RootPage {
 			jQImage.attr('src', jQImage.attr('data-src-backup'));
 			jQImage.attr('data-src', jQImage.attr('data-src-backup'));
 		});
-	}
-
-	protected correctFileType(extension: string) {
-		let extensionWithDot = (extension.charAt(0) === '.') ? extension : `.${extension}`;
-		this.jQuery('img.image').toArray().forEach(image => {
-			let jQImage = this.jQuery(image);
-			let src = jQImage.attr('src');
-
-			let newSrc = src.replace(/\.(\w+)$/, extensionWithDot);
-
-			jQImage.attr('src', newSrc);
-			jQImage.attr('data-src', newSrc);
-		});
-	}
-
-	@RegisteredAction({ id: 'pa_autocorrect_file_type', label: 'Autocorrect File Type' })
-	public autoCorrectFileType(): void {
-		(<any>(<any>unsafeWindow).pixiv.api.illust.detail([this.illustId], {})).then((response:any) => {this.correctFileType(response.body[this.illustId].illust_ext) });
-	}
-
-	@RegisteredAction({ id: 'pa_alter_file_type', label: 'New File Type' })
-	public alterFileType(): void {
-		let input = unsafeWindow.prompt('Choose a new filetype to try', '.png');
-		this.correctFileType(input);
 	}
 
 	@RegisteredAction({ id: 'pa_download_manga_images', label: 'Download All', icon: 'download2' })
