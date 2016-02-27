c5fcc7f3f8c0411730d726683693aab585382394
Paarth <paarthk@gmail.com>
2016-02-27 00:22:32 -0500

    Added events to components

--- src/dom/component.ts
+++ src/dom/component.ts
@@ -7,6 +7,18 @@ export interface Component {
 export abstract class AbstractComponent implements Component{
 	public children:Component[];
 	public abstract render():JQuery;
+
+	protected subscribers: { [id: string]: ((param:AbstractComponent) => any)[] }
+
+	public shout (event:string) {
+		this.subscribers['event'].forEach(f => f(this));
+	}
+	public listen (event:string, callback:(param:AbstractComponent) => any) {
+		if(this.subscribers['event'] == undefined){
+			this.subscribers['event'] = [];
+		}
+		this.subscribers['event'].push(callback);
+	}
 }
 
 export function renderComponent(component:Component):JQuery {
