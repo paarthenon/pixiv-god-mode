3b4e0ebda0b556ad86c12c76a88a591bebf8c32a
Paarth <paarthk+gh@gmail.com>
2016-03-09 01:14:30 -0500

    Attempting to get things going with a fresh start

---
+++ packages/pixiv-assistant-common/proto.ts
@@ -0,0 +1,44 @@
+export const Features = {
+	OpenToRepo: 'openRepo',
+	OpenToArtist: 'openArtist',
+	DownloadImage: 'downloadImage',
+	DownloadManga: 'downloadManga',
+	DownloadAnimation: 'downloadAnimation',
+	ImageExists: 'imageExists',
+	ImageExistsForArtist: 'imageExists'
+}
+
+export module Model {
+	export interface Artist {
+		id: number
+		name: string
+	}
+	export interface Image {
+		id: number
+		page?: number
+		ext?: string
+	}
+}
+
+export module Messages {
+	export interface Response<T> {
+		success :boolean
+	}
+	export interface PositiveResponse<T> extends Response<T> {
+		data :T
+	}
+	export interface NegativeResponse<T> extends Response<T> {
+		errors: string[]
+	}
+
+	export function isPositiveResponse<T>(resp:Response<T>): resp is PositiveResponse<T> {
+		return resp.success;
+	}
+	export function isNegativeResponse<T>(resp:Response<T>): resp is NegativeResponse<T> {
+		return !resp.success;
+	}
+
+	export interface ImageRequest {
+		image :Model.Image
+	}
+}
