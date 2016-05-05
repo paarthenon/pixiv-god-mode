d180e883c89091a46b674955ec1eb015fe4acffd
Paarth <paarthk@gmail.com>
2016-05-04 23:49:05 -0400

    Reorganization

---
+++ vendor/chrome/options/bootstrap.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import {MainPanel} from './popup/mainPanel'
+
+ReactDOM.render(<div></div>, document.getElementById('content'));
--- vendor/chrome/popup/bootstrap.tsx
+++ vendor/chrome/popup/bootstrap.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import {MainPanel} from './popup/mainPanel'
+import {MainPanel} from './mainPanel'
 
 ReactDOM.render(<MainPanel />, document.getElementById('content'));
