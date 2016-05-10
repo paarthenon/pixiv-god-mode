0c9e086491aa3e5cc95a6601b110b1edc5874c2e
Paarth <paarthk@gmail.com>
2016-05-09 23:14:28 -0400

    Removed Q from chrome vendor config

--- vendor/chrome/config.ts
+++ vendor/chrome/config.ts
@@ -1,5 +1,4 @@
 import IConfig from '../../src/IConfig'
-import * as Q from 'q'
 
 type potentialData = boolean|string|number|Object
 
@@ -7,30 +6,35 @@ interface configValue {
 	data: potentialData
 }
 
-function safeResolve<T>(q:Q.Deferred<T>, value: T) {
-	if (!chrome.runtime.lastError) {
-		q.resolve(value);
-	} else {
-		q.reject(chrome.runtime.lastError.message);
-	}
+function handleError<T>(value: T) {
+	return new Promise((resolve, reject) => {
+		if (!chrome.runtime.lastError) {
+			resolve(value);
+		} else {
+			reject(chrome.runtime.lastError.message);
+		}
+	});
+	
 }
 
 export default class Config implements IConfig {
 	public keys():Promise<string[]> {
-		let q = Q.defer<string[]>();
-		chrome.storage.local.get(null, contents => safeResolve(q, Object.keys(contents)));
-		return Promise.resolve(q.promise);
+		return new Promise(resolve => {
+			chrome.storage.local.get(null, contents => resolve(handleError(Object.keys(contents))));
+		});
 	}
 
 	public get(key:string):Promise<potentialData> {
-		let q = Q.defer<potentialData>();
-		chrome.storage.local.get(key, contents => safeResolve(q, JSON.parse(contents[key]).data));
-		return Promise.resolve(q.promise);
+		return new Promise(resolve => 
+			chrome.storage.local.get(key, contents => resolve(handleError(JSON.parse(contents[key]).data)))
+		);
 	}
 
 	public set(key:string, value:potentialData) {
-		let q = Q.defer<void>();
-		chrome.storage.local.set({ [key]: value }, () => safeResolve(q, undefined));
-		return Promise.resolve(q.promise);
+		return new Promise<void>(resolve => 
+			chrome.storage.local.set({ [key]: value }, () => resolve(
+				handleError(undefined).then(()=>Promise.resolve())) // necessary to return Promise<void>
+			)
+		);
 	}
 }
