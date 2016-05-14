da4e1c68833112a8e78a19b6d74c074fb5259b09
Paarth <paarthk@gmail.com>
2016-05-14 14:35:34 -0400

    Extracted SingleConfigDictionary

--- src/utils/dict.ts
+++ src/utils/dict.ts
@@ -5,40 +5,7 @@ import * as ghUtils from './github'
 import IDictionary from '../core/IDictionary'
 import * as log4js from 'log4js'
 
-type stringMap = { [id: string]: string }
-
-class SingleConfigDict implements IDictionary {
-	protected dict: Promise<stringMap>;
-	constructor(protected config:IConfig, protected configKey:string) {
-		// creates and sets default dict on initialization
-		this.dict = config.get(configKey)
-			.then(dict => dict || {})
-			.catch(error => {
-				logger.warn('Unable to get dictionary. Creating');
-				return {};
-			});
-	}
-	public get keys(){
-		return this.dict.then(dict => Object.keys(dict));
-	}
-	public get(key:string):Promise<string> {
-		return this.dict.then(dict => dict[key]);
-	}
-	public set(key:string, value:string):void {
-		this.dict = this.dict.then(dict => {
-			if (value) {
-				dict[key] = value;
-			} else {
-				delete dict[key];
-			}
-
-			return Promise.resolve(dict).then(dict => { 
-				this.config.set(this.configKey, dict); 
-				return dict; 
-			});
-		});
-	}
-}
+import SingleConfigDict from '../core/singleConfigDictionary'
 
 class DictBroker {
 	constructor(protected dictionaries: IDictionary[]) { }
