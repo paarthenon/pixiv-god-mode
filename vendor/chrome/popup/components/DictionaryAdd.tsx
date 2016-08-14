4439ba85c8f14de392b2a4d4a8ae695576fd83d5
p <paarthk+pixiv@gmail.com>
2016-08-14 06:13:41 -0400

    DictionaryAdd component automatically populates the japanese tag if available

--- vendor/chrome/popup/components/DictionaryAdd.tsx
+++ vendor/chrome/popup/components/DictionaryAdd.tsx
@@ -2,15 +2,36 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import * as Bootstrap from 'react-bootstrap'
 
+
+import * as pathUtils from '../../../../src/utils/path'
+
+import * as chromeUtils from '../../utils'
+
 export class DictionaryAdd extends React.Component<{onAdd:(key:string,value:string)=>any},void> {
+	constructor(props:any){
+		super(props);
+
+		chromeUtils.getCurrentTab().then(tab => {
+			let potentialTag = pathUtils.getPotentialTag(tab.url);
+			console.log('found potentialTag', potentialTag);
+			if (potentialTag !== '' && this.japaneseInput.value === '') {
+				this.japaneseInput.value = potentialTag;
+			}
+		})
+		
+	}
+	public get japaneseInput() {
+		return ReactDOM.findDOMNode(this.refs['japanese']) as HTMLInputElement;
+	}
+	public get translationInput() {
+		return ReactDOM.findDOMNode(this.refs['translation']) as HTMLInputElement;
+	}
 	public handleAdd(){
-		let japaneseInput: any = ReactDOM.findDOMNode(this.refs['japanese']);
-		let translationInput: any = ReactDOM.findDOMNode(this.refs['translation']);
-		this.props.onAdd(japaneseInput.value, translationInput.value)
+		this.props.onAdd(this.japaneseInput.value, this.translationInput.value)
 
 		// Clear the form now that the entry has been saved.
-		japaneseInput.value = '';
-		translationInput.value = '';
+		this.japaneseInput.value = '';
+		this.translationInput.value = '';
 	}
 	public render() {
 		return <Bootstrap.Panel header="Add Translation">
