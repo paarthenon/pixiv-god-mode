97ef021db8915c11fe03252fa3e062ac3d4a5d57
Paarth <paarthk+gh@gmail.com>
2016-08-23 16:04:20 -0400

    Included react

---
+++ packages/pixiv-assistant-server/src/www/bootstrap.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import * as log4js from 'log4js'
+
+let logger = log4js.getLogger('Bootstrap');
+
+
+ReactDOM.render(<span>Reacted</span>, document.getElementById('content'));
+
