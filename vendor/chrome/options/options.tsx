77d301cb6d11f12d75dcbb18274713d2ef4eb179
p <paarthk+pixiv@gmail.com>
2016-10-06 18:25:38 -0400

    removed unused panels, minor updates to options.

--- vendor/chrome/options/options.tsx
+++ vendor/chrome/options/options.tsx
@@ -2,5 +2,17 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import {SettingsPanel} from 'vendor/chrome/popup/settingsPanel'
+import {DictionaryJSON} from 'vendor/chrome/popup/exportDict'
 
-ReactDOM.render(<SettingsPanel />, document.getElementById('content'));
+class SettingsPage extends React.Component<void, void> {
+    style = {
+        width: '800px'
+    }
+    public render() {
+        return <div style={this.style}>
+            <SettingsPanel />
+            <DictionaryJSON />
+        </div>
+    }
+}
+ReactDOM.render(<SettingsPage />, document.getElementById('content'));
