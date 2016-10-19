11fdf804702a8dbb412d0a15144484da8d68803d
p <paarthk+pixiv@gmail.com>
2016-10-19 03:58:32 -0400

    minor styling

--- src/components/dictionary/duplicateKeyReporter.tsx
+++ src/components/dictionary/duplicateKeyReporter.tsx
@@ -3,10 +3,15 @@ import * as Bootstrap from 'react-bootstrap'
 
 export class DuplicateKeyReporter extends React.Component<{dupes: string[], removeAction:Function}, void> {
 	public render() {
-		return (this.props.dupes === undefined) ? null :
-			<div> 
+		return (this.props.dupes === undefined) ? null : <Bootstrap.InputGroup style={{width: '100%'}}>
+			<Bootstrap.InputGroup.Addon style={{width: 'auto'}}>
 				Found <strong>{this.props.dupes.length}</strong> duplicate entries. 
-				{(this.props.dupes.length > 0) ? <Bootstrap.Button onClick={() => this.props.removeAction()} small>Revert Duplicates</Bootstrap.Button> : null}
-			</div>
+			</Bootstrap.InputGroup.Addon>
+			{(this.props.dupes.length > 0) ? 
+				<Bootstrap.InputGroup.Button>
+					<Bootstrap.Button onClick={() => this.props.removeAction()} small>Revert Duplicates</Bootstrap.Button>
+				</Bootstrap.InputGroup.Button> 
+			: null }
+		</Bootstrap.InputGroup>
 	}
 }
