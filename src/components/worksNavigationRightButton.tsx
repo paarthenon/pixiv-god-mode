911b84da50aa24fd83980884bcf6b51e4b276878
paarth <paarthk+gh@gmail.com>
2016-11-24 01:51:47 -0500

    Converted to bootstrap button for openintabs

--- src/components/worksNavigationRightButton.tsx
+++ src/components/worksNavigationRightButton.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
+import * as Bootstrap from 'react-bootstrap'
+
+import {InjectBootstrap} from 'src/components/util/injectBootstrap'
 
 export const WorksNavbarRightButton : React.StatelessComponent<{text:string,clickAction:Function}> = props => 
 	<ul style={{float: 'right'}}>
 		<li>
-			<a onClick={() => props.clickAction()} style={{cursor: 'pointer'}}>
+			<InjectBootstrap>
+			<Bootstrap.Button onClick={() => props.clickAction()} style={{cursor: 'pointer'}} small>
 				{props.text}
-			</a>
+			</Bootstrap.Button>
+			</InjectBootstrap>
 		</li>
 	</ul>
